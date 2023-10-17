08559f7a9fb20b6e35ef4d785bf5543d0ae2e2a8
Suganya394002 <144356135+Suganya394002@users.noreply.github.com>
2023-10-17 14:26:28 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,18 +1,48 @@
-#Define the player class
-class player:
-  def play(self):
-    print("The player is playing cricket.")
-#Define the Batsman class,derived from player
-class Batsman(player):
-    def play(self):
-        print("The batsman is batting.")
-#Define the Bowlerclass,derived from player
-class Bowler(player):
-    def play(self):
-        print("The bowler is bowling.")
- #create objects of Batsman and Bowler classes
-batsman=Batsman()
-bowler=Bowler()
-#call the play() method for each object 
-batsman.play()
-bowler.play()
+'''
+Implement a function called sort_students that takes a list of students objects as input and sorts the 
+list based on their CGPA (Cumulative Grade Point Average) in descending order. Each student object 
+has the following attributes: name (string), roll_number (string),and cgpa (float). Test the function 
+with different input lists of student.
+'''
+
+class Student:
+  
+  def __init__(self,name, roll_number, cgpa):
+    self.name = name
+    self.roll_number = roll_number
+    self.cgpa = cgpa
+
+
+def sort_students(student_list):
+  # Sort the list of students in descending order of CGPA
+  sorted_students = sorted(student_list,
+                   key=lambda student: student.cgpa,
+                   reverse=True)
+  # Syntax - lambda arg:exp
+  return sorted_students
+  
+
+# Example Usage:
+students = [
+    Student("Hari", "A123", 7.8),
+    Student("Srikanth", "A124",8.9),
+    Student("Saumya", "A125", 9.1),
+    Student("Mahidhar", "A126",9.9),
+]
+
+sorted_students= sort_students(students)
+
+# Print the sorted list of students 
+for student in sorted_students:
+  print("Name: {}, Roll number: {}, CGPA: {}".format(student.name,
+                                    student.roll_number,
+                        student.cgpa))
+                                    
+
+
+
+    
+                             
+                             
+    
+  
